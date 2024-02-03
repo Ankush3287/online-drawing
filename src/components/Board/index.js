c00c8241e30b926cdd219b4af3bfec4f6b661a1e
Ankush <ankushsolanki3287@gmail.com>
2024-02-03 12:03:50 +0530

    setup socket connection

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -2,6 +2,7 @@ import { MENU_ITEMS } from "@/constants";
 import { useEffect, useRef, useLayoutEffect } from "react"
 import { useDispatch, useSelector } from 'react-redux'
 import {menuItemClick, actionItemClick} from '@/slice/menuSlice'
+import { socket } from "@/socket";
 
 const Board = () => {
     const dispatch = useDispatch();
@@ -79,6 +80,11 @@ const Board = () => {
         canvas.addEventListener('mousedown', handleMouseDown);
         canvas.addEventListener('mousemove', handleMouseMove);
         canvas.addEventListener('mouseup', handleMouseUp);
+
+        // Socket Code
+        socket.on("connect", () => {
+            console.log("Client connected");
+          });
 
         return () => {
             canvas.removeEventListener('mousedown', handleMouseDown);
